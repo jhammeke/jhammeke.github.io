aa497e9aa92e79fa79272d16860dcc7338689a47
jhammeke <38965073+jhammeke@users.noreply.github.com>
2020-11-09 11:50:20 -0500

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Skills section
+$('.skills-content').waypoint(function() {
+    $('.progress .progress-bar').each(function() {
+      $(this).css("width", $(this).attr("aria-valuenow") + '%');
+    });
+  }, {
+    offset: '80%'
+  });
+
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+  }
+
+  // Initiate aos_init() function
+  aos_init();
